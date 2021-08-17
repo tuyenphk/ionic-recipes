a0a9ff12d6c56e2efba2557c9952b282652fe756
Tuyen Pham <tuh39709@temple.edu>
2021-08-17 14:36:06 -0400

    lifecycle hooks

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Recipe } from './recipe.modal';
 import { RecipesService } from './recipes.service';
 
@@ -7,13 +7,36 @@ import { RecipesService } from './recipes.service';
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
 })
-export class RecipesPage implements OnInit {
+export class RecipesPage implements OnInit, OnDestroy {
 
   recipes: Recipe[];
   constructor(private recipeService: RecipesService) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.getAllRecipes();
+    console.log('ngOnInit');
+    console.log(this.recipes);
+  }
+
+  ionViewWillEnter(){
+    console.log('ionViewWillEnter');
+    this.recipes = this.recipeService.getAllRecipes();
+  }
+
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter');
+  }
+
+  ionViewWillLeave(){
+    console.log('ionViewWillLeave');
+  }
+
+  ionViewDidLeave(){
+    console.log('ionViewDidLeave');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
   }
 
 }
